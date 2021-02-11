97d3b918c58b1b65a3de120c63e3c5960365fada
Kris Peklaric <peklarick@gmail.com>
2021-02-11 21:54:26 +0100

    fix - env variable name

--- movies-front/src/Services/BaseAxios.js
+++ movies-front/src/Services/BaseAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_KEY + "v1/",
+  baseURL: process.env.REACT_APP_API_URL + "v1/",
   headers: {
     "Content-type": "application/json",
   },
